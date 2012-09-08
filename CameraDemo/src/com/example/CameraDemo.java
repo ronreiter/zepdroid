eec6f3098ea225ac65e540defa8b431a72a7771c
Amir Lazarovich <amirlazarovich@gmail.com>
2012-09-08 12:55:21 +0300

    Supporting power on/off

--- CameraDemo/src/com/example/CameraDemo.java
+++ CameraDemo/src/com/example/CameraDemo.java
@@ -33,109 +33,106 @@ import java.io.InputStream;
 import java.util.TimerTask;
 
 public class CameraDemo extends Activity implements ADKManager.Callback {
-	private static final String TAG = "CameraDemo";
+    private static final String TAG = "CameraDemo";
 
     ADKManager mADKManager;
-	Camera camera;
-	Preview preview;
-	Button buttonClick;
-	SocketIO socket = null;
+    Camera camera;
+    Preview preview;
+    Button buttonClick;
+    SocketIO socket = null;
     MediaPlayer player = null;
-	
 
-	/** Called when the activity is first created. */
-	
-	class TakePicTask extends TimerTask{
-		
-		public void run(){
-			
-			preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-		}
-	}
-	
-	private class SentPicTask extends AsyncTask<byte[], Integer, Long> {
 
-		@Override
-		protected Long doInBackground(byte[]... params) {
-			
-			String url = "http://zepdroid.com:8099";
-			
-			Log.d(TAG, " params[0] wrote bytes: " + params[0].length);
-			
-			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost(url);
-			
-			InputStream dataStream = new ByteArrayInputStream(params[0]);
-			
-			try {
-				Log.d(TAG, " before sending 0  " + dataStream.available() );
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			InputStreamEntity reqEntity;
-			try {
-				reqEntity = new InputStreamEntity(dataStream, dataStream.available());
-				reqEntity.setContentType("binary/octet-stream");
-			    
-			    Log.d(TAG, " before sending 1" + reqEntity.getContentLength() );
-			    
-			    //reqEntity.setChunked(true); // Send in multiple parts if needed
-			    httppost.setEntity(reqEntity);
-			    try {
-					HttpResponse response = httpclient.execute(httppost);
-				} catch (ClientProtocolException e) {
-					Log.d(TAG, "ClientProtocolException");
-					e.printStackTrace();
-				} catch (IOException e) {
-					Log.d(TAG, "IOException");
-					e.printStackTrace();
-				} catch (Exception e) {
-					Log.d(TAG, "Exception");
-					e.printStackTrace();
-				}
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-		    
-			
-			Log.d(TAG, "Async op");
-			return null;
-		}
+    /**
+     * Called when the activity is first created.
+     */
 
-	
+    class TakePicTask extends TimerTask {
 
-	    
-	 }
-	
-	
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+        public void run() {
+
+            preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        }
+    }
+
+    private class SentPicTask extends AsyncTask<byte[], Integer, Long> {
+
+        @Override
+        protected Long doInBackground(byte[]... params) {
+
+            String url = "http://zepdroid.com:8099";
+
+            Log.d(TAG, " params[0] wrote bytes: " + params[0].length);
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+
+            InputStream dataStream = new ByteArrayInputStream(params[0]);
+
+            try {
+                Log.d(TAG, " before sending 0  " + dataStream.available());
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+
+            InputStreamEntity reqEntity;
+            try {
+                reqEntity = new InputStreamEntity(dataStream, dataStream.available());
+                reqEntity.setContentType("binary/octet-stream");
+
+                Log.d(TAG, " before sending 1" + reqEntity.getContentLength());
+
+                //reqEntity.setChunked(true); // Send in multiple parts if needed
+                httppost.setEntity(reqEntity);
+                try {
+                    HttpResponse response = httpclient.execute(httppost);
+                } catch (ClientProtocolException e) {
+                    Log.d(TAG, "ClientProtocolException");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Log.d(TAG, "IOException");
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    Log.d(TAG, "Exception");
+                    e.printStackTrace();
+                }
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+
+
+            Log.d(TAG, "Async op");
+            return null;
+        }
+
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
         mADKManager = new ADKManager(this, this);
-		preview = new Preview(this);
-		((FrameLayout) findViewById(R.id.preview)).addView(preview);
-		
-		//Timer getPic = new Timer();
-		//getPic.schedule(new TakePicTask(), 1000*3);
-		
-		
-		try {
-			socket = new SocketIO("http://zepdroid.com:8099/");
-			
-			
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+        preview = new Preview(this);
+        ((FrameLayout) findViewById(R.id.preview)).addView(preview);
+
+        //Timer getPic = new Timer();
+        //getPic.schedule(new TakePicTask(), 1000*3);
+
+
+        try {
+            socket = new SocketIO("http://zepdroid.com:8099/");
+
+
+        } catch (Exception e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
         socket.connect(new IOCallback() {
-   
 
 
             @Override
@@ -149,31 +146,31 @@ public class CameraDemo extends Activity implements ADKManager.Callback {
             }
 
 
-			@Override
-			public void onMessage(String data, IOAcknowledge ack) {
-				// TODO Auto-generated method stub
-				
-				
-				Log.d(TAG, "onMessage");
-				
-				
-			}
-			
+            @Override
+            public void onMessage(String data, IOAcknowledge ack) {
+                // TODO Auto-generated method stub
 
-			@Override
-			public void onMessage(JSONObject json, IOAcknowledge ack) {
-				
-				// TODO Auto-generated method stub
-				Log.d(TAG, "onMessagejson");
-				
-			}
 
-			@Override
-			public void on(String event, IOAcknowledge ack, Object... args) {
-				Log.d(TAG, "on");
-				// TODO Auto-generated method stub
-				
-				Log.d(TAG, event);
+                Log.d(TAG, "onMessage");
+
+
+            }
+
+
+            @Override
+            public void onMessage(JSONObject json, IOAcknowledge ack) {
+
+                // TODO Auto-generated method stub
+                Log.d(TAG, "onMessagejson");
+
+            }
+
+            @Override
+            public void on(String event, IOAcknowledge ack, Object... args) {
+                Log.d(TAG, "on");
+                // TODO Auto-generated method stub
+
+                Log.d(TAG, event);
 
                 if (args[0].equals("ngn1start") && args[1].equals("up")) {
                     Log.d(TAG, "ng1Start up");
@@ -207,89 +204,98 @@ public class CameraDemo extends Activity implements ADKManager.Callback {
 
                 }
 
-                if (args[0].equals("connect") && args[1].equals("up")) {
-                    Log.d(TAG, "connect up");
-
+                if (args[0].equals("power_on") && args[1].equals("up")) {
+                    Log.d(TAG, "power on");
+                    sendCommand(ADKManager.COMMAND_STAND_BY,
+                            ADKManager.ACTION_POWER_ON,
+                            null);
                 }
-				
-				if(args[0].equals("forward") && args[1].equals("up")){
-					Log.d(TAG, "up: forward");
-					Object[] resp = {"hello", "world"};
-					socket.emit("test", resp);
+
+                if (args[0].equals("power_off") && args[1].equals("up")) {
+                    Log.d(TAG, "power off");
+                    sendCommand(ADKManager.COMMAND_STAND_BY,
+                            ADKManager.ACTION_POWER_OFF,
+                            null);
+                }
+
+                if (args[0].equals("forward") && args[1].equals("up")) {
+                    Log.d(TAG, "up: forward");
+                    Object[] resp = {"hello", "world"};
+                    socket.emit("test", resp);
                     byte speed = (byte) 255;
                     sendCommand(
                             ADKManager.COMMAND_MOTOR_1,
                             ADKManager.ACTION_POWER_ON,
                             new byte[]{ADKManager.DIRECTION_LEFT, (byte) speed});
-				}
-				if(args[0].equals("forward") && args[1].equals("down")){
-					Log.d(TAG, "down: forward");
-				}
-				if(args[0].equals("left") && args[1].equals("up")){
-					Log.d(TAG, "up: left");
+                }
+                if (args[0].equals("forward") && args[1].equals("down")) {
+                    Log.d(TAG, "down: forward");
+                }
+                if (args[0].equals("left") && args[1].equals("up")) {
+                    Log.d(TAG, "up: left");
                     sendCommand(
                             ADKManager.COMMAND_ROTATE,
                             ADKManager.ACTION_END_ROTATE,
                             null);
-				}
-				if(args[0].equals("left") && args[1].equals("down")){
-					Log.d(TAG, "down: left");
+                }
+                if (args[0].equals("left") && args[1].equals("down")) {
+                    Log.d(TAG, "down: left");
                     sendCommand(
                             ADKManager.COMMAND_ROTATE,
                             ADKManager.ACTION_START_ROTATE,
                             new byte[]{ADKManager.DIRECTION_LEFT});
-				}
-				if(args[0].equals("right") && args[1].equals("up")){
-					Log.d(TAG, "up: right");
+                }
+                if (args[0].equals("right") && args[1].equals("up")) {
+                    Log.d(TAG, "up: right");
                     sendCommand(
                             ADKManager.COMMAND_ROTATE,
                             ADKManager.ACTION_END_ROTATE,
                             null);
-				}
-				if(args[0].equals("right") && args[1].equals("down")){
-					Log.d(TAG, "down: right");
+                }
+                if (args[0].equals("right") && args[1].equals("down")) {
+                    Log.d(TAG, "down: right");
                     sendCommand(
                             ADKManager.COMMAND_ROTATE,
                             ADKManager.ACTION_START_ROTATE,
                             new byte[]{ADKManager.DIRECTION_RIGHT});
-				}
-				if(args[0].equals("back") && args[1].equals("up")){
-					Log.d(TAG, "up: back");
+                }
+                if (args[0].equals("back") && args[1].equals("up")) {
+                    Log.d(TAG, "up: back");
                     byte speed = (byte) 255;
                     sendCommand(
                             ADKManager.COMMAND_MOTOR_1,
                             ADKManager.ACTION_POWER_ON,
                             new byte[]{ADKManager.DIRECTION_RIGHT, (byte) speed});
-				}
-				if(args[0].equals("back") && args[1].equals("down")){
-					Log.d(TAG, "down: back");
-				}
-				
-				if(args[0].equals("elevate_up") && args[1].equals("up")){
-					Log.d(TAG, "up: elevate_up");
+                }
+                if (args[0].equals("back") && args[1].equals("down")) {
+                    Log.d(TAG, "down: back");
+                }
+
+                if (args[0].equals("elevate_up") && args[1].equals("up")) {
+                    Log.d(TAG, "up: elevate_up");
                     byte speed = (byte) 255;
                     sendCommand(
                             ADKManager.COMMAND_MOTOR_2,
                             ADKManager.ACTION_POWER_ON,
                             new byte[]{ADKManager.DIRECTION_LEFT, (byte) speed});
-				}
-				if(args[0].equals("elevate_up") && args[1].equals("down")){
-					Log.d(TAG, "down: elevate_up");
-				}
-				
-				if(args[0].equals("elevate_down") && args[1].equals("up")){
-					Log.d(TAG, "up: elevate_down");
+                }
+                if (args[0].equals("elevate_up") && args[1].equals("down")) {
+                    Log.d(TAG, "down: elevate_up");
+                }
+
+                if (args[0].equals("elevate_down") && args[1].equals("up")) {
+                    Log.d(TAG, "up: elevate_down");
                     byte speed = (byte) 255;
                     sendCommand(
                             ADKManager.COMMAND_MOTOR_2,
                             ADKManager.ACTION_POWER_ON,
                             new byte[]{ADKManager.DIRECTION_RIGHT, (byte) speed});
-				}
-				if(args[0].equals("elevate_down") && args[1].equals("down")){
-					Log.d(TAG, "down: elevate_down");
-				}
+                }
+                if (args[0].equals("elevate_down") && args[1].equals("down")) {
+                    Log.d(TAG, "down: elevate_down");
+                }
 
-                if(args[0].equals("music") && args[1].equals("up")){
+                if (args[0].equals("music") && args[1].equals("up")) {
                     Log.d(TAG, "up: music");
                     // toggle playing
                     if (CameraDemo.this.player.isPlaying()) {
@@ -299,34 +305,33 @@ public class CameraDemo extends Activity implements ADKManager.Callback {
                         CameraDemo.this.player.start();
                     }
                 }
-                if(args[0].equals("music") && args[1].equals("down")){
+                if (args[0].equals("music") && args[1].equals("down")) {
                     Log.d(TAG, "down: music");
                 }
-				
-				if(args[0].equals("picture") && args[1].equals("up")){
-					Log.d(TAG, "up: picture");
-					
-					preview.camera.takePicture(shutterCallback, rawCallback, null, jpegCallback);
-					
-					//Object[] resp = {"hello"};
-					//socket.emit("get_img", resp);
-				}
-				
-				if(args[0].equals("picture") && args[1].equals("down")){
-					Log.d(TAG, "down: picture");
-				}
-				
-						
-				
-			}
 
-			@Override
-			public void onError(SocketIOException socketIOException) {
-				Log.d(TAG, "onError");
-			    }
-        	}
+                if (args[0].equals("picture") && args[1].equals("up")) {
+                    Log.d(TAG, "up: picture");
+
+                    preview.camera.takePicture(shutterCallback, rawCallback, null, jpegCallback);
+
+                    //Object[] resp = {"hello"};
+                    //socket.emit("get_img", resp);
+                }
+
+                if (args[0].equals("picture") && args[1].equals("down")) {
+                    Log.d(TAG, "down: picture");
+                }
+
+
+            }
+
+            @Override
+            public void onError(SocketIOException socketIOException) {
+                Log.d(TAG, "onError");
+            }
+        }
         );
-			
+
         try {
             player = new MediaPlayer();
             AssetFileDescriptor afd = CameraDemo.this.getResources().openRawResourceFd(R.raw.wholelottalove);
@@ -341,9 +346,9 @@ public class CameraDemo extends Activity implements ADKManager.Callback {
         }
 
         buttonClick = (Button) findViewById(R.id.buttonClick);
-		buttonClick.setOnClickListener( new OnClickListener() {
-			public void onClick(View v) {
-				//preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        buttonClick.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                //preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
                 //AudioManager audioManager = (AudioManager) getSystemService(CameraDemo.AUDIO_SERVICE);
                 //audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
 
@@ -357,10 +362,10 @@ public class CameraDemo extends Activity implements ADKManager.Callback {
                 }
 
             }
-		});
+        });
 
-		Log.d(TAG, "onCreate'd");
-	}
+        Log.d(TAG, "onCreate'd");
+    }
 
     /**
      * Send command to the ADK
@@ -400,46 +405,50 @@ public class CameraDemo extends Activity implements ADKManager.Callback {
     }
 
 
-	ShutterCallback shutterCallback = new ShutterCallback() {
-		public void onShutter() {
-			Log.d(TAG, "onShutter'd");
-		}
-	};
+    ShutterCallback shutterCallback = new ShutterCallback() {
+        public void onShutter() {
+            Log.d(TAG, "onShutter'd");
+        }
+    };
 
-	/** Handles data for raw picture */
-	PictureCallback rawCallback = new PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera camera) {
-			Log.d(TAG, "onPictureTaken - raw");
-		}
-	};
+    /**
+     * Handles data for raw picture
+     */
+    PictureCallback rawCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d(TAG, "onPictureTaken - raw");
+        }
+    };
 
-	/** Handles data for jpeg picture */
-	PictureCallback jpegCallback = new PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera camera) {
-			FileOutputStream outStream = null;
-			//try {
-				// write to local sandbox file system
+    /**
+     * Handles data for jpeg picture
+     */
+    PictureCallback jpegCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            FileOutputStream outStream = null;
+            //try {
+            // write to local sandbox file system
 //				outStream = CameraDemo.this.openFileOutput(String.format("%d.jpg", System.currentTimeMillis()), 0);	
-				// Or write to sdcard
-				//outStream = new FileOutputStream(String.format("/sdcard/%d.jpg", System.currentTimeMillis()));	
-				//outStream.write(data);
-				//outStream.close();
-				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
-				System.out.print("Length:" + data.length);
-				new SentPicTask().execute(data);
-				
-				//Object[] resp = {data};
-				//socket.emit("get_img", resp);
-					
-				
-			/*} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-			}*/
-			Log.d(TAG, "onPictureTaken - jpeg");
-		}
-	};
+            // Or write to sdcard
+            //outStream = new FileOutputStream(String.format("/sdcard/%d.jpg", System.currentTimeMillis()));
+            //outStream.write(data);
+            //outStream.close();
+            Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+            System.out.print("Length:" + data.length);
+            new SentPicTask().execute(data);
+
+            //Object[] resp = {data};
+            //socket.emit("get_img", resp);
+
+
+            /*} catch (FileNotFoundException e) {
+                   e.printStackTrace();
+               } catch (IOException e) {
+                   e.printStackTrace();
+               } finally {
+               }*/
+            Log.d(TAG, "onPictureTaken - jpeg");
+        }
+    };
 
 }
